f97d624f9e120311b6ecca814fd13275fe78a9ad
tunglam123 <minkboy96@gmail.com>
2016-08-23 23:19:35 +0700

    First Commit

---
+++ database/orders_temp.php
@@ -0,0 +1,2 @@
+<?php exit; ?>
+a:1:{s:13:"1471499240188";a:2:{i:13;a:3:{s:9:"iQuantity";i:1;s:6:"fPrice";s:6:"299.00";s:5:"sName";s:16:"Mountain Hotel 3";}i:7;a:3:{s:9:"iQuantity";i:1;s:6:"fPrice";s:5:"64.00";s:5:"sName";s:22:"Mandi (Vibram, Nubuck)";}}}
